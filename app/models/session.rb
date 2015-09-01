2b4b0eeae01e996289e64adac072231a044ab6bc
Caleb Francis <calebonesimus@gmail.com>
2015-09-01 15:55:56 -0400

    Find all restaurantes near coordinates

--- app/models/session.rb
+++ app/models/session.rb
@@ -6,7 +6,9 @@ class Session
   field :route, type: Hash
   field :restaurants, type: Hash
 
-  def get_restaurants
+  def get_restaurants(coords_array)
+    coords_hash = { latitude: coords_array.first, longitude: coords_array.last }
+    Yelp.client.search_by_coordinates(coords_hash)
   end
 
 
