c2f43439fed9cf826a8c79fd609e200e9971047a
Daniel Beyrer <beyrerdn@gmail.com>
2015-09-09 09:09:02 -0400

    Adds a few sessions controller tests and changed location of the save if/else construct. Tests pass.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -10,14 +10,28 @@ RSpec.describe SessionsController, type: :controller do
       post :create, { origin: "Indianapolis", destination: "Chicago", api_key: "1" }
       expect(response).to be_success
     end
+    it 'gives a json response containing restaurant array' do
+      post :create, { origin: "Indianapolis", destination: "Chicago", api_key: "1" }
+      expect_json_types(restaurants: :array)
+    end
+    it 'does not create a session object without an api_key' do
+      Session.all.destroy_all
+      post :create, { origin: "Indianapolis", destination: "Chicago" }
+      expect(Session.all.count).to eq(0)
+    end
   end
 
   describe "GET /sessions" do
-    it 'GET request returns a session object' do
+    it 'returns a session object' do
       post :create, { origin: "Indianapolis", destination: "Chicago", api_key: "1" }
       get :show, { api_key: "1" }
       expect_json(origin: "Indianapolis")
       expect(response).to be_success
+    end
+    it 'gives a json response containing restaurant array' do
+      post :create, { origin: "Indianapolis", destination: "Chicago", api_key: "1" }
+      get :show, { api_key: "1" }
+      expect_json_types(restaurants: :array)
     end
   end
 
