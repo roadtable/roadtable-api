2c50e8cdce1b2195ce50d6675c35813343a0457d
Caleb Francis <calebonesimus@gmail.com>
2015-09-10 14:23:43 -0400

    Refactor sessions controller, model, and tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :set_session
+
+  def set_session
+    if params[:api_key]
+      if Session.where(:api_key => params[:api_key])
+        @session = Session.where(:api_key => params[:api_key]).first
+      else
+        render json: "Invalid api key."
+      end
+    else
+      render json: "An api key is needed for this action."
+    end
+  end
+
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,52 +1,57 @@
 class Session
   include Mongoid::Document
-  field :origin, type: String
-  field :destination, type: String
   field :api_key, type: String
-  field :route, type: Hash
-  field :restaurants, type: Array, default: []
-  field :list, type: Array, default: []
+  has_one :route
+  embeds_many :restaurants, as: :chosen_restaurants
 
+
+  ####
+  #
+  # MOVING TO POLYPOINT
+  #
+  ####
   # Gets five restaurants near every nth poly point
   # Converts data to hash
-  def get_restaurants(points)
-    i = 0
-    increment = 10
-    while i < points.length
-      # Convert point array into hash
-      point_hash = { latitude: points[i].first, longitude: points[i].last }
+  # def get_restaurants(points)
+  #   i = 0
+  #   increment = 10
+  #   while i < points.length
+  #     # Convert point array into hash
+  #     point_hash = { latitude: points[i].first, longitude: points[i].last }
+  #
+  #     # Returns BurstStruct object that Yelp creates
+  #     point_results = Yelp.client.search_by_coordinates(point_hash, { limit: 5 })
+  #
+  #     # Reformat that Yelp object into hash with only the data we need
+  #     point_results.businesses.each do |restaurant|
+  #       self.restaurants << {
+  #         id: restaurant.id,
+  #         name: restaurant.name,
+  #         rating: restaurant.rating,
+  #         display_phone: restaurant.respond_to?(:display_phone) ? restaurant.phone : "",
+  #         categories: restaurant.respond_to?(:categories) ? categories_to_string(restaurant.categories) : "",
+  #         mobile_url: restaurant.respond_to?(:mobile_url) ? restaurant.mobile_url : "",
+  #         rating_img_url: restaurant.respond_to?(:rating_img_url) ? restaurant.rating_img_url : "",
+  #         image_url: restaurant.respond_to?(:image_url) ? restaurant.image_url : "http://s3-media4.fl.yelpcdn.com/assets/srv0/yelp_styleguide/c73d296de521/assets/img/default_avatars/business_90_square.png",
+  #         display_address: restaurant.respond_to?(:display_address) ? restaurant.display_address : "",
+  #         polypoint: { latitude: points[i].first, longitude: points[i].last }
+  #       }
+  #     end
+  #     i += increment
+  #   end
+  # end
 
-      # Returns BurstStruct object that Yelp creates
-      point_results = Yelp.client.search_by_coordinates(point_hash, { limit: 5 })
+  # TO RESTAURANT
+  # def categories_to_string(array)
+  #   single_categories = array.collect do |item|
+  #     item[0]
+  #   end
+  #   single_categories.join(", ")
+  # end
 
-      # Reformat that Yelp object into hash with only the data we need
-      point_results.businesses.each do |restaurant|
-        self.restaurants << {
-          id: restaurant.id,
-          name: restaurant.name,
-          rating: restaurant.rating,
-          display_phone: restaurant.respond_to?(:display_phone) ? restaurant.phone : "",
-          categories: restaurant.respond_to?(:categories) ? categories_to_string(restaurant.categories) : "",
-          mobile_url: restaurant.respond_to?(:mobile_url) ? restaurant.mobile_url : "",
-          rating_img_url: restaurant.respond_to?(:rating_img_url) ? restaurant.rating_img_url : "",
-          image_url: restaurant.respond_to?(:image_url) ? restaurant.image_url : "http://s3-media4.fl.yelpcdn.com/assets/srv0/yelp_styleguide/c73d296de521/assets/img/default_avatars/business_90_square.png",
-          display_address: restaurant.respond_to?(:display_address) ? restaurant.display_address : "",
-          polypoint: { latitude: points[i].first, longitude: points[i].last }
-        }
-      end
-      i += increment
-    end
-  end
-
-  def categories_to_string(array)
-    single_categories = array.collect do |item|
-      item[0]
-    end
-    single_categories.join(", ")
-  end
-
-  def remove_duplicate_restaurants
-    self.restaurants.uniq!{ |restaurant| restaurant[:id] }
-  end
+  # TO ROUTE
+  # def remove_duplicate_restaurants
+  #   self.restaurants.uniq!{ |restaurant| restaurant[:id] }
+  # end
 
 end
