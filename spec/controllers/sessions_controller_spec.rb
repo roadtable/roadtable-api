ffa3a3e942166107cfa916f55c289b678a486ff5
Caleb Francis <calebonesimus@gmail.com>
2015-09-11 17:00:50 -0400

    Sessions test now work

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -21,31 +21,32 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "GET /sessions" do
     it 'gives a json response containing chosen restaurant array' do
-      post :create, { origin: "Indianapolis", destination: "Chicago", api_key: "1" }
-      get :show, { api_key: "1" }
+      session = Session.create(api_key: "2")
+      session.route = Route.create(origin: "Indianapolis", destination: "Chicago", session_id: session.id)
+      get :show, { api_key: "2" }
       expect_json_types(:array)
     end
   end
 
   describe "UPDATE /sessions" do
     it 'adds restaurant to chosen restaurants' do
-      session = Session.create(api_key: "1")
+      session = Session.create(api_key: "3")
       session.route = Route.create(origin: "Indianapolis", destination: "Chicago", session_id: session.id)
       restaurant = session.route.available_restaurants.first
-      patch :update, { api_key: "1", akushon: "add", yelp_id: restaurant.yelp_id }
+      patch :update, { api_key: "3", akushon: "add", yelp_id: restaurant.yelp_id }
 
-      expect(session.chosen_restaurants.first).to eq(restaurant)
-      expect(session.chosen_restaurants.count).to eq(1)
+      expect(session.reload.chosen_restaurants.first).to eq(restaurant)
+      expect(session.reload.chosen_restaurants.count).to eq(1)
     end
 
     it 'removes restaurant from chosen restaurants' do
-      session = Session.create(api_key: "1")
+      session = Session.create(api_key: "4")
       session.route = Route.create(origin: "Indianapolis", destination: "Chicago", session_id: session.id)
       restaurant = session.route.available_restaurants.first
       session.chosen_restaurants.batch_insert([restaurant])
-      # Delete the restaurant
-      patch :update, { api_key: "1", akushon: "delete", yelp_id: restaurant.yelp_id }
-      expect(session.chosen_restaurants.count).to eq(0)
+      patch :update, { api_key: "4", akushon: "delete", yelp_id: restaurant.yelp_id }
+
+      expect(session.reload.chosen_restaurants.count).to eq(0)
     end
   end
 
