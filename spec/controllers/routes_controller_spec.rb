e46ba16e35f86d77024eb76b034dee512cfc06bd
Daniel Beyrer <beyrerdn@gmail.com>
2015-09-11 11:27:48 -0400

    Adds Routes controller#show test.

--- spec/controllers/routes_controller_spec.rb
+++ spec/controllers/routes_controller_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe RoutesController, type: :controller do
   describe "GET /routes" do
-    it '' do
-
+    it 'it returns an available_restaurants array' do
+      Session.all.destroy_all
+      post :create, { controller: "sessions", origin: "Indianapolis", destination: "Chicago", api_key: "1" }
+      expect(response).to be_success
+      expect(Session.all.count).to eq(1)
+      get :show, {api_key: "1"}
+      expect_json_types(:array)
     end
   end
 
