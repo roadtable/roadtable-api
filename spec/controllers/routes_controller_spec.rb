290c0e4479e645f56f588eaa69bd7f5971b9941e
Daniel Beyrer <beyrerdn@gmail.com>
2015-09-11 11:49:34 -0400

    Changes routes_controller test to create route and session.

--- spec/controllers/routes_controller_spec.rb
+++ spec/controllers/routes_controller_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe RoutesController, type: :controller do
   describe "GET /routes" do
     it 'it returns an available_restaurants array' do
-      Session.all.destroy_all
-      post :create, { controller: "sessions", origin: "Indianapolis", destination: "Chicago", api_key: "1" }
-      expect(response).to be_success
-      expect(Session.all.count).to eq(1)
+      @session = Session.create(api_key: "1")
+      @route = Route.create(origin: "Indianapolis", destination: "Bloomington", session_id: @session.id)
       get :show, {api_key: "1"}
       expect_json_types(:array)
     end
