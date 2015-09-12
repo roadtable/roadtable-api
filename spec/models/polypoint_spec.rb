52ca311c94cb7d72f07e80eca72e5ffdbe16c468
Daniel Beyrer <beyrerdn@gmail.com>
2015-09-12 01:22:00 -0400

    Adds model test for polypoint as well as travis.yml.

--- spec/models/polypoint_spec.rb
+++ spec/models/polypoint_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Polypoint, type: :model do
-  describe 'create polypoint' do
-    it ' ' do
-
-    end
-
+  it 'saves with five restaurants' do
+    polypoint = Polypoint.create(coordinates: [40.075558, -83.095400])
+    expect(polypoint.nearby_restaurants.count).to eq(5)
   end
 end
