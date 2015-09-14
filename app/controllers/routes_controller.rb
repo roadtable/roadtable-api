adc0bc41dda10099446abcb9c7460bdfee8cd89f
Daniel Beyrer <beyrerdn@gmail.com>
2015-09-14 17:35:38 -0400

    Changes error handling in routes.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -5,8 +5,11 @@ class RoutesController < ApplicationController
       @filtered_restaurants = @session.route.available_restaurants.select do |r|
         r.name.include?(filter) || r.name.include?(filter.capitalize) || r.categories.include?(filter) || r.categories.include?(filter.capitalize)
       end
-
-      render json: @filtered_restaurants
+      if @filtered_restaurants.empty?
+        render json: {errors: "No restaurants were found under '#{filter}'."}
+      else
+        render json: @filtered_restaurants
+      end
     else
       @restaurants = @session.route.available_restaurants
       render json: @restaurants
