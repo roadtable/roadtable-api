401e1b8c684d9a07f6d957207f3edece48cbe567
Caleb Francis <calebonesimus@gmail.com>
2015-09-14 21:22:35 -0400

    Adds a session model test

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Session, type: :model do
+  it 'does not save without api key' do
+    Session.destroy_all
+    session = Session.create
+    expect(session).to be_invalid
+    expect(session).to validate_presence_of(:api_key)
+  end
 end
